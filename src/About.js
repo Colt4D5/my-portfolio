84cac37fd7a422e8db8493ba04aa57649637630b
Colton Allen <allen.colton@hotmail.com>
2021-04-08 00:22:04 -0700

    implemented dynamic projects

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <h1 className="section_heading">about</h1>
         <div className="about_content">
           <div className="column column_left">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam possimus dolorem cumque. Velit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam possimus dolorem cumque. Velit.</p>
+            <p>i am an accredited freelance <strong>copy editor</strong> in multiple books in the amazon book store, entrepreneur as co-owner of <strong>pink city cake studio</strong> in riverside, ca, awarded several years best custom bakery in riverside, and aspiring <strong>web developer</strong>. </p>
           </div>
           <div className="column column_center">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam possimus dolorem cumque. Velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam.</p>
--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
 // const [message, setMessage] = useState(null);
 
   return (
-    <section id="contact" className="contact_container">
+    <section id="contact" className="section contact_container">
       <h1 className="section_heading">love what you see? let's work together.</h1>
       <div className="inner contact_inner">
         <form action="">
@@ -15,7 +15,7 @@ const Contact = () => {
           <input type="email" id="name" name="name" placeholder="name"/><br/>
           <input type="text" id="email" name="email" placeholder="email address"/><br/>
           <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="phone number (xxx-xxx-xxxx)"/><br/>
-          <input type="text" id="message" name="message" placeholder="message me for job inquiries, more information, or whatever else"/><br/>
+          <textarea type="text" id="message" name="message" cols="40" rows="5" placeholder="message me for job inquiries, more information, or whatever else"></textarea><br/>
           <input type="submit" id="submit" name="submit"/>
         </form>
       </div>
