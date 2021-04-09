ea91ef9ecc070d8a6eedc2b5b3a93d2eb7518a15
Colton Allen <allen.colton@hotmail.com>
2021-04-08 17:19:39 -0700

    testing contact form

--- src/Contact.js
+++ src/Contact.js
@@ -10,10 +10,10 @@ const Contact = () => {
     <section id="contact" className="section contact_container">
       <h1 className="section_heading">love what you see? let's work together.</h1>
       <div className="inner contact_inner">
-        <form action="">
+        <form id="contact-form" method="post" action="public/email.php">
           <h2>contact me</h2>
-          <input type="email" id="name" name="name" placeholder="name"/><br/>
-          <input type="text" id="email" name="email" placeholder="email address"/><br/>
+          <input type="text" id="name" name="name" placeholder="name"/><br/>
+          <input type="email" id="email" name="email" placeholder="email address"/><br/>
           <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="phone number (xxx-xxx-xxxx)"/><br/>
           <textarea type="text" id="message" name="message" cols="40" rows="5" placeholder="message me for job inquiries, more information, or whatever else"></textarea><br/>
           <input type="submit" id="submit" name="submit" value="submit"/>
