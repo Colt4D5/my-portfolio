c72c0592dced3c7525efcfd8a3ab42ca293e8c6e
Colton Allen <allen.colton@hotmail.com>
2021-04-08 22:15:06 -0700

    implemented functional contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,23 +1,54 @@
-// import { useState } from 'react';
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-// const [name, setName] = useState(null);
-// const [email, setEmail] = useState(null);
-// const [phone, setPhone] = useState(null);
-// const [message, setMessage] = useState(null);
+  const sendEmail = e => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7u4l888', 'template_k9hf1i8', e.target, 'user_seeCm1suBqHAREJo6MzIm')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
 
   return (
     <section id="contact" className="section contact_container">
       <h1 className="section_heading">love what you see? let's work together.</h1>
       <div className="inner contact_inner">
-        <form id="contact-form" method="post" action="public/email.php">
+        <form id="contact-form" onSubmit={sendEmail}>
           <h2>contact me</h2>
-          <input type="text" id="name" name="name" placeholder="name"/><br/>
-          <input type="email" id="email" name="email" placeholder="email address"/><br/>
-          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="phone number (xxx-xxx-xxxx)"/><br/>
-          <textarea type="text" id="message" name="message" cols="40" rows="5" placeholder="message me for job inquiries, more information, or whatever else"></textarea><br/>
-          <input type="submit" id="submit" name="submit" value="submit"/>
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            placeholder="name" /> 
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            placeholder="email address" />
+          <input 
+            type="tel" 
+            id="phone" 
+            name="phone" 
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+            placeholder="phone number xxx-xxx-xxxx" />
+          <textarea 
+            type="text" 
+            id="message" 
+            name="message" 
+            cols="40" 
+            rows="5" 
+            placeholder="message me for job inquiries, more information, or whatever else" ></textarea>
+          <input 
+            type="submit" 
+            id="submit" 
+            name="submit" 
+            value="submit"/>
         </form>
+        <p className="attribution">*implemented with Email.js</p>
       </div>
     </section>
   );
--- src/Technologies.js
+++ src/Technologies.js
@@ -10,6 +10,7 @@ const Technologies = () => {
           <Skill comfort="70" lang="CSS" url="https://img.icons8.com/color/250/000000/css3.png"/>
           <Skill comfort="85" lang="JavaScript" url="https://img.icons8.com/color/240/000000/html-5--v2.png"/>
           <Skill comfort="50" lang="React" url="https://img.icons8.com/color/250/000000/react-native.png"/>
+          <Skill comfort="70" lang="TypeScript" url="https://img.icons8.com/color/250/000000/typescript.png"/>
           <Skill comfort="90" lang="jQuery" url="https://img.icons8.com/ios-filled/250/000000/jquery.png"/>
           <Skill comfort="95" lang="Sass" url="https://img.icons8.com/color/250/000000/sass.png"/>
           <Skill comfort="20" lang="Bootstrap" url="https://img.icons8.com/color/256/000000/bootstrap.png"/>
