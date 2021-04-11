79effcb71fe218b7cb975c91fc6fa36e46c9f38a
Colton Allen <allen.colton@hotmail.com>
2021-04-10 21:54:17 -0700

    added netflix clone project

--- src/About.js
+++ src/About.js
@@ -5,13 +5,13 @@ const About = () => {
         <h1 className="section_heading">about</h1>
         <div className="about_content">
           <div className="column column_left">
-            <p>i am an accredited freelance <strong>copy editor</strong> in multiple books in the amazon book store, entrepreneur as co-owner of <strong>pink city cake studio</strong> in riverside, ca, awarded several years best custom bakery in riverside, and aspiring <strong>web developer</strong>.</p>
+            <p>technology in all of its facets has always been a passion of mine and seeing what its capabilities are. i have invested much of my time in taking courses to educate myself from industry professionals, as well as coding on my own, all in my aspiration to establish a life-long career in web development. i find the wide world of coding to be endlessly fascinating, which satisfies my love for problem-solving and creating things that i can be proud of, that help people globally, and advance the fast-moving world of tech.</p>
           </div>
           <div className="column column_center">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam possimus dolorem cumque. Velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam.</p>
+          <p>in my quest to achieve this goal to become a web developer, i have completed several courses and even receiving a certificate of completion for an extensive 68-hour course through Udemy, to which i attribute much of my knowledge of the wonderful language of javascript. in addition, i am presently learning the react framework through another course that i have purchased, completed deved’s javascript course through his website, consumed countless hours of tutorials, and have purchased several other courses of languages that i seek to learn in order to be the best asset possible to the team with which i eventually work.</p>
           </div>
           <div className="column column_right">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus molestiae blanditiis accusamus inventore modi quibusdam possimus dolorem cumque. Velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ducimus.</p>
+          <p>up until this point in my life, i have found myself as a freelance <strong>copy editor</strong> accredited in multiple books that are featured in the amazon book store, as well as an entrepreneur as co-owner/operator of <strong>pink city cake studio</strong> in riverside, ca, awarded several years best custom bakery in riverside.</p>
           </div>
         </div>
       </div>
