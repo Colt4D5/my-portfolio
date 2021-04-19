6fcbe6b23b110a9e8ceecf92362a6f61d7835f2a
Colton Allen <allen.colton@hotmail.com>
2021-04-19 10:51:56 -0700

    added download feature for resume

--- src/Links.js
+++ src/Links.js
@@ -4,26 +4,52 @@ const Links = () => {
       <div className="inner">
         <div className="location">
           <h1>location</h1>
-          <p>riverside, ca<br/>
-          951.756.0273</p>
+          <p>
+            riverside, ca
+            <br />
+            951.756.0273
+          </p>
         </div>
         <div className="resume">
-          <button className="resume_box">
+          <a
+            href="/assets/files/resume.pdf"
+            target="_blank"
+            className="resume_box"
+            download
+          >
             <h1>resume</h1>
-            <img src="https://img.icons8.com/material/24/ffffff/download--v1.png" alt=""/>
-          </button>
+            <img
+              src="https://img.icons8.com/material/24/ffffff/download--v1.png"
+              alt=""
+            />
+          </a>
         </div>
         <div className="links">
           <h1>links</h1>
           <div className="icons">
-            <a href="www.linkedin.com/in/colton-a-allen"><img alt="linkedin icon" src="https://img.icons8.com/material-rounded/48/ffffff/linkedin--v1.png"/></a>
-            <a href="https://github.com/Colt4D5"><img alt="github icon" src="https://img.icons8.com/material-outlined/48/ffffff/github.png"/></a>
-            <a href="https://codepen.io/Colt4D5"><img alt="codepen icon" src="https://img.icons8.com/material/48/ffffff/codepen.png"/></a>
+            <a href="www.linkedin.com/in/colton-a-allen">
+              <img
+                alt="linkedin icon"
+                src="https://img.icons8.com/material-rounded/48/ffffff/linkedin--v1.png"
+              />
+            </a>
+            <a href="https://github.com/Colt4D5">
+              <img
+                alt="github icon"
+                src="https://img.icons8.com/material-outlined/48/ffffff/github.png"
+              />
+            </a>
+            <a href="https://codepen.io/Colt4D5">
+              <img
+                alt="codepen icon"
+                src="https://img.icons8.com/material/48/ffffff/codepen.png"
+              />
+            </a>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Links;
