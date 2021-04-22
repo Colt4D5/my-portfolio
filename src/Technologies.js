6dc542b7490c2cd1867d0d7b9017e09e5796e082
Colton Allen <allen.colton@hotmail.com>
2021-04-22 14:58:16 -0700

    updated technologies

--- src/Technologies.js
+++ src/Technologies.js
@@ -8,13 +8,15 @@ const Technologies = () => {
         <div className="skills">
           <Skill comfort="95" lang="HTML" url="https://img.icons8.com/color/250/000000/html-5.png" index="0" />
           <Skill comfort="70" lang="CSS" url="https://img.icons8.com/color/250/000000/css3.png" index="1" />
-          <Skill comfort="85" lang="JavaScript" url="https://img.icons8.com/color/240/000000/html-5--v2.png" index="2" />
-          <Skill comfort="50" lang="React" url="https://img.icons8.com/color/250/000000/react-native.png" index="3" />
-          <Skill comfort="70" lang="TypeScript" url="https://img.icons8.com/color/250/000000/typescript.png" index="4" />
-          <Skill comfort="90" lang="jQuery" url="https://img.icons8.com/ios-filled/250/000000/jquery.png" index="5" />
-          <Skill comfort="95" lang="Sass" url="https://img.icons8.com/color/250/000000/sass.png" index="6" />
-          <Skill comfort="20" lang="Bootstrap" url="https://img.icons8.com/color/256/000000/bootstrap.png" index="7" />
-          <Skill comfort="10" lang="NodeJS" url="https://img.icons8.com/color/240/000000/nodejs.png" index="8" />
+          <Skill comfort="85" lang="JavaScript" url="https://img.icons8.com/color/250/000000/html-5--v2.png" index="2" />
+          <Skill comfort="60" lang="React" url="https://img.icons8.com/color/250/000000/react-native.png" index="3" />
+          <Skill comfort="40" lang="PHP 8" url="https://img.icons8.com/ios-filled/250/787CB5/php-logo.png" index="4" />
+          <Skill comfort="70" lang="TypeScript" url="https://img.icons8.com/color/250/000000/typescript.png" index="5" />
+          <Skill comfort="90" lang="jQuery" url="https://img.icons8.com/ios-filled/250/000000/jquery.png" index="6" />
+          <Skill comfort="95" lang="Sass" url="https://img.icons8.com/color/250/000000/sass.png" index="7" />
+          <Skill comfort="55" lang="Wordpress" url="https://img.icons8.com/nolan/250/wordpress.png" index="8" />
+          <Skill comfort="40" lang="Bootstrap" url="https://img.icons8.com/color/250/000000/bootstrap.png" index="9" />
+          <Skill comfort="10" lang="NodeJS" url="https://img.icons8.com/color/250/000000/nodejs.png" index="10" />
         </div>
       </div>
     </section>
